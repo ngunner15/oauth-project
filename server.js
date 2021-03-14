eead5d174d3c08c8e37175130cdf6b5faab5cb30
Nisarg Vadgama <nisargvv@gmail.com>
2021-03-14 19:10:38 -0400

    get token from callback using fetch

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 require('dotenv').config(); // add this line to use dotenv package
 
@@ -11,7 +12,28 @@ app.get('/login/github', (req, res) => {
   res.redirect(url);
 });
 
-app.get('/login/github/callback', (req, res) => {});
+async function getAccessToken(code) {
+  const res = await fetch('https://github.com/login/oauth/access_token', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+  });
+  const data = await res.text();
+  const params = new URLSearchParams(data);
+  return params.get('access_token');
+}
+
+app.get('/login/github/callback', async (req, res) => {
+  const code = req.query.code;
+  const token = await getAccessToken(code);
+  res.json({token});
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log('Listening on http://localhost:' + PORT));
